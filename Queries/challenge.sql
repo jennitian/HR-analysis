7343dbff9ab95cfd6855f085c10814702340feb9
Jennifer Tian <jennifertian@Jennifers-MacBook-Pro.local>
2019-11-14 21:46:29 -0800

    challenge completed

--- Queries/challenge.sql
+++ Queries/challenge.sql
@@ -1,24 +1,54 @@
-SELECT ce.emp_no, ce.first_name, ce.last_name,
-	t.title, s.from_date, s.salary
-INTO retiring_pos_salary
-FROM current_emp as ce
-LEFT JOIN titles as t
-ON (ce.emp_no = t.emp_no)
-LEFT JOIN salaries as s
-ON (ce.emp_no = s.emp_no);
+--creating data table of current employees born in 1965 with titles and salaries
+SELECT ce.emp_no, ce.first_name, ce.last_name, t.title, t.from_date, s.salary
+INTO retirees_titles
+FROM salaries AS s
+RIGHT JOIN titles as t
+ON (s.emp_no = t.emp_no)
+RIGHT JOIN current_emp AS ce
+ON (ce.emp_no = s.emp_no)
+ORDER BY emp_no;
 
-SELECT * FROM retiring_pos_salary;
+SELECT * FROM retirees_titles;
 
+--retirees list no duplicates
 SELECT emp_no, first_name, last_name, title, from_date, salary
-INTO retiring_no_dup
+INTO retirees_titles_unique
 FROM (SELECT emp_no, first_name, last_name, title,
-	from_date, salary, ROW_NUMBER() OVER (PARTITION BY (emp_no)) rn
-	 FROM retiring_pos_salary)
-	 tmp WHERE rn = 1;
+	from_date, salary, ROW_NUMBER() OVER (PARTITION BY (emp_no) ORDER BY from_date DESC) rn
+	 FROM retirees_titles) AS rt
+	 WHERE rn = 1;
+	 
+--or alternatively
+SELECT emp_no, first_name, last_name, title, from_date, salary
+--INTO retirees_titles_unique
+FROM (SELECT emp_no, first_name, last_name, title,
+	from_date, salary, ROW_NUMBER() OVER (PARTITION BY (first_name, last_name) ORDER BY from_date DESC) rn
+	 FROM retirees_titles) AS rt
+	 WHERE rn = 1;
+	 
+SELECT * FROM retirees_titles_unique;
 
-     Create a new table that contains the following information:
-Employee number
-First and last name
-Title
-from_date and to_date
-Note: The hire date needs to be between January 1, 1965 and December 31, 1965. Also, make sure only current employees are included in this list.
+--counting number of current employees eligible for retirement
+SELECT COUNT(current_emp)
+FROM current_emp;
+
+--creating mentors list
+SELECT e.emp_no, e.first_name, e.last_name, t.title, de.from_date, de.to_date
+INTO mentors_list
+FROM employees as e
+RIGHT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+RIGHT JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+--creating unique mentors list
+SELECT emp_no, first_name, last_name, title, from_date, to_date
+INTO mentors_list_unique
+FROM (SELECT emp_no, first_name, last_name, title,
+	from_date, to_date, ROW_NUMBER() OVER (PARTITION BY (emp_no) ORDER BY from_date DESC) AS rn
+	 FROM mentors_list) AS ml
+	 WHERE rn = 1;
+SELECT * FROM mentors_list;
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# jennifertian_HR-analysis
+# Pewlett-Hackard Human Resources Analysis
+module 7 challenge
+## Goal
+The goal of this project was to query various employee information databases to find some information on the employees at PH who are eligible for retirement and generate a list of potential mentors who are eligible for younger or new employees.
+## Findings
+We found that currently, there are 33,118 employees who are eligible for retirement out of 240,120 total employees. Out of these we identified 1549 current employees born around 1965 who could potentially be mentors for younger employees.
+## Database Visualisation
+To query this I created the following data base diagram using quickDBD.
+![alt test](Pictures/EmployeeDB.png)
+##Query Codes
+### List of Retirees with Title and Salary
+SELECT ce.emp_no, ce.first_name, ce.last_name, t.title, t.from_date, s.salary
+INTO retirees_titles
+FROM salaries AS s
+RIGHT JOIN titles as t
+ON (s.emp_no = t.emp_no)
+RIGHT JOIN current_emp AS ce
+ON (ce.emp_no = s.emp_no)
+ORDER BY emp_no;
+#### Output
+![retirees](Pictures/retirees_titles.png)
+### Unique List of Retirees with Title and Salary
+SELECT emp_no, first_name, last_name, title, from_date, salary
+INTO retirees_titles_unique
+FROM (SELECT emp_no, first_name, last_name, title,
+	from_date, salary, ROW_NUMBER() OVER (PARTITION BY (emp_no) ORDER BY from_date DESC) rn
+	 FROM retirees_titles) AS rt
+	 WHERE rn = 1;
+#### Output
+![retirees unique](Pictures/retirees_titles_unique.png)
+### List of mentors from 1965
+SELECT e.emp_no, e.first_name, e.last_name, t.title, de.from_date, de.to_date
+INTO mentors_list
+FROM employees as e
+RIGHT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+RIGHT JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+#### Output
+![mentors](Pictures/mentors_list.png)
+### List of unique mentors from 1965
+SELECT emp_no, first_name, last_name, title, from_date, to_date
+INTO mentors_list_unique
+FROM (SELECT emp_no, first_name, last_name, title,
+	from_date, to_date, ROW_NUMBER() OVER (PARTITION BY (emp_no) ORDER BY from_date DESC) AS rn
+	 FROM mentors_list) AS ml
+	 WHERE rn = 1;
+#### Output
+![mentors unique](Pictures/mentors_list_unique.png)
